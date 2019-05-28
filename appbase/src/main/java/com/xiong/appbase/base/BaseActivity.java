317290910bb1cd04005b14049f9b3b077d72de8f
ncxnbrwa <18395201462@163.com>
2019-05-28 14:31:37 +0800

    添加权限申请代码

--- appbase/src/main/java/com/xiong/appbase/base/BaseActivity.java
+++ appbase/src/main/java/com/xiong/appbase/base/BaseActivity.java
@@ -1,10 +1,17 @@
 package com.xiong.appbase.base;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.telecom.Call;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +27,9 @@ import com.xiong.appbase.http.UploadImgEngine;
 import com.xiong.appbase.http.UploadImgService;
 import com.xiong.appbase.utils.DLog;
 import com.xiong.appbase.utils.ScreenUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import me.yokeyword.fragmentation.SupportActivity;
 import okhttp3.ResponseBody;
@@ -133,6 +143,106 @@ public abstract class BaseActivity extends SupportActivity {
         super.onDestroy();
     }
 
+    //**********************************权限申请封装**********************************//
+    private int REQUEST_CODE_PERMISSION = 0x00099;
+
+    //请求权限,外部调用
+    public void requestPermission(String[] permissions, int requestCode) {
+        this.REQUEST_CODE_PERMISSION = requestCode;
+        if (checkPermissions(permissions)) {
+            permissionSuccess(REQUEST_CODE_PERMISSION);
+        } else {
+            List<String> needPermissions = getDeniedPermissions(permissions);
+            ActivityCompat.requestPermissions(this, needPermissions.toArray(new String[needPermissions.size()]), REQUEST_CODE_PERMISSION);
+        }
+    }
+
+    //检测所有的权限是否都已授权
+    private boolean checkPermissions(String[] permissions) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //获取权限集中需要申请权限的列表
+    private List<String> getDeniedPermissions(String[] permissions) {
+        List<String> needRequestPermissionList = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) !=
+                    PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                needRequestPermissionList.add(permission);
+            }
+        }
+        return needRequestPermissionList;
+    }
+
+
+    //系统请求权限回调
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (verifyPermissions(grantResults)) {
+                permissionSuccess(REQUEST_CODE_PERMISSION);
+            } else {
+                permissionFail(REQUEST_CODE_PERMISSION);
+                showTipsDialog();
+            }
+        }
+    }
+
+    //确认所有的权限是否都已授权
+    private boolean verifyPermissions(int[] grantResults) {
+        for (int grantResult : grantResults) {
+            if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //显示权限提示对话框
+    private void showTipsDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("提示信息")
+                .setMessage("当前应用缺少必要权限，该功能暂时无法使用。如若需要，请单击【确定】按钮前往设置中心进行权限授权。")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startAppSettings();
+                    }
+                }).show();
+    }
+
+    //启动当前应用设置页面
+    private void startAppSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        startActivity(intent);
+    }
+
+    //获取权限成功,子类回调
+    public void permissionSuccess(int requestCode) {
+        DLog.d(getClass().getSimpleName(), "获取权限成功=" + requestCode);
+    }
+
+    //权限获取失败,子类回调
+    public void permissionFail(int requestCode) {
+        DLog.d(getClass().getSimpleName(), "获取权限失败=" + requestCode);
+    }
 
     //放入传值的参数
     public void setInternalActivityParam(String key, Object object) {
